dbcff1220e8f5693f34b2c377e930ed2f4c72c99
Airat <airatmak@gmail.com>
2021-11-29 20:27:06 +0300

    hw5

--- src/lab5/Main.java
+++ src/lab5/Main.java
@@ -1,11 +1,18 @@
 package lab5;
 
+import jdk.jshell.execution.LoaderDelegate;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void modelExceptions() {
@@ -56,8 +63,84 @@ public class Main {
         stackOverFlow();
     }
 
+    public static void datesCalculating() {
+        LocalDate now = LocalDate.now();
+
+        LocalDate lincolnBirth = LocalDate.of(1809, Month.FEBRUARY, 12);
+        LocalDate lincolnDeath = LocalDate.of(1855, Month.APRIL, 15);
+        System.out.println("He died when he was " + ChronoUnit.YEARS.between(lincolnBirth, lincolnDeath));
+        System.out.println("He lived " + ChronoUnit.DAYS.between(lincolnBirth, lincolnDeath) + " days");
+        System.out.println();
+
+        LocalDate cumberbatchBirth = LocalDate.of(1976, Month.JULY, 19);
+        System.out.println("Cumberbatch born in " + (cumberbatchBirth.isLeapYear() ? "leap year" : "non-leap year"));
+        System.out.println("He is " + ChronoUnit.DECADES.between(cumberbatchBirth, now) + " decades");
+        System.out.println("It was " + cumberbatchBirth.plusYears(21).getDayOfWeek());
+        System.out.println();
+
+        LocalTime bostonDep = LocalTime.of(13, 45);
+        LocalTime newYorkArr = LocalTime.of(19, 25);
+        System.out.println("Ride longs " + ChronoUnit.MINUTES.between(bostonDep, newYorkArr) + " minutes");
+        System.out.println("Arrival time: " + newYorkArr.plusHours(1).plusMinutes(19));
+        System.out.println();
+
+        LocalDateTime boston = LocalDateTime.of(2021, Month.MARCH, 24, 21, 15);
+        System.out.println("Plane arrives at " + boston.plusHours(4).plusMinutes(15));
+        System.out.println("Plane with delay arrives at " + boston.plusHours(4).plusMinutes(27).plusHours(4).plusMinutes(15));
+        System.out.println();
+
+        LocalDate september = LocalDate.of(2021, Month.SEPTEMBER, 1);
+        LocalDate semesterStart = september
+                .with(TemporalAdjusters.firstInMonth(DayOfWeek.TUESDAY))
+                .with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
+        System.out.println("First day of semester is " + semesterStart);
+        LocalDate semesterEnd = LocalDate.of(2022, Month.JUNE, 25);
+        long weeks = ChronoUnit.WEEKS.between(semesterStart, semesterEnd);
+        weeks -= 4; //vacations
+        LocalDate lastWeekStart = semesterEnd.with(TemporalAdjusters.previousOrSame(DayOfWeek.TUESDAY));
+        long days = ChronoUnit.DAYS.between(lastWeekStart, semesterEnd);
+        if (days == 7)
+            days -= 2;
+        if (days == 6)
+            days -= 1;
+        System.out.println("There are " + (weeks * 5 + days) + " days in school");
+        System.out.println();
+
+        LocalDateTime today = LocalDateTime.of(LocalDate.now(), LocalTime.of(13, 30));
+        System.out.println("Week's meeting is at " + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY)));
+        System.out.println();
+    }
+
+    public static void flights() {
+        ZoneId BOS = ZoneId.of("America/New_York");
+        ZoneId SFO = ZoneId.of("America/Los_Angeles");
+        ZoneId BLR = ZoneId.of("Asia/Calcutta");
+
+        System.out.println("Flight 123");
+        LocalDateTime localStart123 = LocalDateTime.of(2014, Month.JUNE, 13, 22, 30);
+        ZonedDateTime start123 = ZonedDateTime.of(localStart123, SFO);
+        System.out.println(start123.withZoneSameInstant(BOS));
+        System.out.println(start123.plusHours(5).plusMinutes(30).withZoneSameInstant(BOS));
+        System.out.println(start123.plusHours(5).plusMinutes(30));
+        System.out.println();
+
+        System.out.println("Flight 456");
+        LocalDateTime localStart456 = LocalDateTime.of(2014, Month.JUNE, 28, 22, 30);
+        ZonedDateTime start456 = ZonedDateTime.of(localStart456, SFO);
+        System.out.println(start456.plusHours(22).withZoneSameInstant(BLR)
+                .isBefore(ZonedDateTime.of(2014, 6, 29, 9, 0, 0, 0, BLR)));
+        System.out.println();
+
+        System.out.println("Flight 123");
+        LocalDateTime localStart = LocalDateTime.of(2014, Month.NOVEMBER, 1, 22, 30);
+        ZonedDateTime start = ZonedDateTime.of(localStart, SFO);
+        System.out.println(start123.withZoneSameInstant(BOS).plusHours(5).plusMinutes(30));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         modelExceptions();
-        stackOverFlow();
+        datesCalculating();
+        flights();
     }
 }
